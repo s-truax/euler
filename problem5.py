6caba0ca41d26e317701d3431ffa757574aa9dd1
Sam Truax <samtruax@fortressiq.com>
2020-09-23 15:21:46 -0700

    Found general solution? Add memoization next

--- problem5.py
+++ problem5.py
@@ -1,4 +1,6 @@
+from functools import reduce
 from itertools import count
+from operator import mul
 
 def first_true(pred, iterable, default=None):
     """Reference:
@@ -28,6 +30,18 @@ def add_dictionaries(d1, d2):
     """
     keys_union = d1.keys() | d2.keys()
     return {k: d1.get(k, 0) + d2.get(k, 0) for k in keys_union}
+   
+def maximize_dictionaries(d1, d2):
+    """Make a new {Any -> int} dictionary using the union of the keys
+       from d1 and d2 and taking the max of the values with identical keys.
+    
+    >>> a = {'one': 1, 'two': 2, 'three': 3}
+    >>> b = {'one': 1, 'two': 20, 'ten': 10}
+    >>> add_dictionaries(a, b)
+    {'one':1, 'two': 20, 'three': 3, 'ten': 10}
+    """
+    keys_union = d1.keys() | d2.keys()
+    return {k: max(d1.get(k, 0), d2.get(k, 0)) for k in keys_union}
 
 def prime_factorize(n):
     """Find the prime factorization of N."""
@@ -39,7 +53,19 @@ def prime_factorize(n):
     return add_dictionaries(left_factors, right_factors)
 
 def solution_helper(n):
-    return
+    """Give the prime factorization for the solution for numbers
+       up to N."""
+    if n == 1:
+        return {1: 1} 
+    n_factors = prime_factorize(n)
+    prev_factorization = solution_helper(n - 1)
+    return maximize_dictionaries(n_factors, prev_factorization)
+
+def solution(n):
+    solution_factorization = solution_helper(n)
+    prime_powers_factorization = {pow(p, e) for p, e in
+                                  solution_factorization.items()}
+    return reduce(mul, prime_powers_factorization, 1)
 
 def sequentially_divisible(m, n):
     """Assert that m is divisible by all positive integers up to and
@@ -51,6 +77,4 @@ def bf_solution(n):
        up to n."""
     positive_ints = count(1)
     return first_true(lambda i: sequentially_divisible(i, n), positive_ints)
-    
-    
     
