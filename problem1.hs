c8b80ad579228c731ceb63b7195e9b245915b53a
Sam Truax <samtruax@fortressiq.com>
2020-09-29 23:00:39 -0700

    translated problem 1 to Haskell

---
+++ problem1.hs
@@ -0,0 +1,13 @@
+import System.Environment(getArgs)
+
+isMultiple :: Int -> Int -> Bool
+isMultiple n 0 = error "No number is a multiple of 0."
+isMultiple n m = n `mod` m == 0
+
+solution :: Int -> Int
+solution 0 = 0
+solution n = sum [k | k <- [1..n],  isMultiple k 3 || isMultiple k 5]
+
+main = do
+    args <- getArgs
+    print $ solution $ read $ head args
