e4d0b9ac014a5bb4806248da9c7d5c360719edf7
Sam Truax <samtruax@fortressiq.com>
2020-10-01 13:54:05 -0700

    refactored and corrected problem7

--- problem7.hs
+++ problem7.hs
@@ -2,36 +2,16 @@
   An implementation of the Sieve of Eratosthenes that uses
   list comprehension (i.e. filtering) to do the bulk of the work.
 
-  Counts the number of primes less than or equal to N.
+  Finds all primes less than or equal to N.
 -}
-filterSieve :: Int -> Int
+filterSieve :: Int -> [Int]
 filterSieve n = let
-    doesNotDivide     x y  = y `mod` x /= 0
-    filterSieveHelper p xs =
-        let sieved = [m | m <- xs, p `doesNotDivide` m]
-            nextP  = head sieved
-        in  if  p^2 > n || null sieved
-                then xs
-                else filterSieveHelper nextP sieved
-    in length $  filterSieveHelper 2 [1..n]
-
-{-
-Not sure why I can't use function composition here:
-
-problem7.hs:16:17: error:
-    • Couldn't match expected type ‘a1 -> t0 a0’
-                  with actual type ‘[Int]’
-    • Possible cause: ‘filterSieveHelper’ is applied to too many arguments
-      In the second argument of ‘(.)’, namely
-        ‘filterSieveHelper 2 [1 .. n]’
-      In the expression: length . filterSieveHelper 2 [1 .. n]
-      In the expression:
-        let
-          doesNotDivide x y = y `mod` x /= 0
-          filterSieveHelper p xs = let ... in ...
-        in length . filterSieveHelper 2 [1 .. n]
-   |
-16 |     in length . filterSieveHelper 2 [1..n]
-   |                 ^^^^^^^^^^^^^^^^^^^^^^^^^^
--}
+    doesNotDivide x y  = y `mod` x /= 0
+    sift []            = []
+    sift (p:xs)        =
+        let sifted = [m | m <- xs, p `doesNotDivide` m]
+        in if 2 * p >= n 
+            then [p]
+                else p : sift sifted
+    in sift [2..n]
 
